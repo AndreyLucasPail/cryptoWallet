25677b737de3f944a434e5dc731d85b57dc683fe
AndreyLucasPail <andreypail@hotmail.com>
2023-12-04 15:22:20 -0300

    update

--- lib/screens/wallet_my_account_screen.dart
+++ lib/screens/wallet_my_account_screen.dart
@@ -1,9 +1,15 @@
 import 'package:crypto_teste/widgets/user_tile.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class MyAccountScreen extends StatelessWidget {
+class MyAccountScreen extends StatefulWidget {
   const MyAccountScreen({super.key});
 
+  @override
+  State<MyAccountScreen> createState() => _MyAccountScreenState();
+}
+
+class _MyAccountScreenState extends State<MyAccountScreen> {
   @override
   Widget build(BuildContext context) {
     return ListView(
@@ -12,9 +18,34 @@ class MyAccountScreen extends StatelessWidget {
           margin: const EdgeInsets.all(16),
           child: Column(
             children: [
-              const CircleAvatar(
-                radius: 50,
+              Stack(
+                alignment: AlignmentDirectional.topStart,
+                children:[ 
+                Container(
+                  height: 120,
+                  width: 120,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(image: AssetImage("assets/homem.png"))
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    
+                  },
+                  child: Container(
+                    height: 30,
+                    width: 30,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color:Colors.red,
+                    ),
+                    child: const Icon(Icons.add, color: Colors.white,size: 20,)
+                  ),
+                ),
+                ]
               ),
+              const SizedBox(height: 8,),
               const Text(
                 "User Name",
                 style: TextStyle(
@@ -22,6 +53,7 @@ class MyAccountScreen extends StatelessWidget {
                   fontSize: 25,
                 ),
               ),
+              const SizedBox(height: 5,),
               const Text(
                 "User@User.com",
                 style: TextStyle(
@@ -30,6 +62,7 @@ class MyAccountScreen extends StatelessWidget {
                   fontWeight: FontWeight.w300
                 ),
               ),
+              const SizedBox(height: 16,),
               SizedBox(
                 height: 45,
                 width: 180,
@@ -48,6 +81,7 @@ class MyAccountScreen extends StatelessWidget {
                   ),
                 ),
               ),
+              const SizedBox(height: 16,),
               Container(height: 1, width: 550, color: Colors.black,),
               UserTile(
                 title: "Phone number:",
@@ -56,33 +90,39 @@ class MyAccountScreen extends StatelessWidget {
               ),
               Container(height: 1, width: 550, color: Colors.black,),
               UserTile(
-                title: "Phone number:",
-                subtitle: "(51)93640-5507",
-                leading: Icon(Icons.phone, color: Colors.grey[900], size: 30,),
+                title: "Address:",
+                subtitle: "Park Avenue",
+                leading: Icon(Icons.home, color: Colors.grey[900], size: 30,),
               ),
               Container(height: 1, width: 550, color: Colors.black,),
               UserTile(
-                title: "Phone number:",
-                subtitle: "(51)93640-5507",
-                leading: Icon(Icons.phone, color: Colors.grey[900], size: 30,),
+                title: "City:",
+                subtitle: "New York",
+                leading: Icon(CupertinoIcons.globe, color: Colors.grey[900], size: 30,),
               ),
               Container(height: 1, width: 550, color: Colors.black,),
               UserTile(
-                title: "Phone number:",
-                subtitle: "(51)93640-5507",
-                leading: Icon(Icons.phone, color: Colors.grey[900], size: 30,),
+                title: "State:",
+                subtitle: "NY",
+                leading: Icon(CupertinoIcons.globe, color: Colors.grey[900], size: 30,),
               ),
               Container(height: 1, width: 550, color: Colors.black,),
               UserTile(
-                title: "Phone number:",
-                subtitle: "(51)93640-5507",
-                leading: Icon(Icons.phone, color: Colors.grey[900], size: 30,),
+                title: "Country:",
+                subtitle: "USA",
+                leading: Icon(CupertinoIcons.globe, color: Colors.grey[900], size: 30,),
               ),
               Container(height: 1, width: 550, color: Colors.black,),
               UserTile(
-                title: "Phone number:",
-                subtitle: "(51)93640-5507",
-                leading: Icon(Icons.phone, color: Colors.grey[900], size: 30,),
+                title: "Date Of Birth:",
+                subtitle: "25/09/1996",
+                leading: Icon(Icons.cake, color: Colors.grey[900], size: 30,),
+              ),
+              Container(height: 1, width: 550, color: Colors.black,),
+              UserTile(
+                title: "Gender:",
+                subtitle: "male",
+                leading: Icon(Icons.male, color: Colors.grey[900], size: 30,),
               ),
             ],
           ),
